14c58a2450de2bb24191801448c6e83e1f4f5e8b
vatsalmanvar <vatsalmanvar@gmail.com>
2022-07-09 15:51:20 +0530

    more childs are done

--- frontend/src/components/Demo.js
+++ frontend/src/components/Demo.js
@@ -10,30 +10,40 @@ const rfStyle = {
 };
 
 const initialNodes = [
-  { id: 'node-1', type: 'textUpdater', position: { x: 50, y: 50 }, data: { value: 123 } },
+  { id: '1', type: 'textUpdater', position: { x: 50, y: 50 }, data: { value: 123, child:0 } },
   
 ];
 // we define the nodeTypes outside of the component to prevent re-renderings
 // you could also use useMemo inside the component
 const nodeTypes = { textUpdater: TextUpdaterNode };
 
+
 function Flow() {
   const [nodes, setNodes] = useState(initialNodes);
   const [edges, setEdges] = useState([]);
-
   const onNodeClick = useCallback((event, node) => {
     setNodes((nodes)=>{
         console.log(node);
+        node.data.child = node.data.child+1;
         return [...nodes,{
-            id:String(Math.random()),
+            id: node.id+node.data.child,
             type:'textUpdater',
-            position:{x:node.position.x,y:node.position.y+50},
-            data:{ value:Math.random()} 
+            position:{x:node.position.x,y:node.position.y+100},
+            data:{ value:Math.random(), child:0},
         }];
     },[]);
     initialNodes.push(node);
     console.log('init nodes',initialNodes);
-    console.log('click node',node);
+    //console.log('child', node.data.child);
+    //console.log('->>> id', node.id);
+
+    setEdges((edges)=>{
+      console.log('click edge',edges);
+      return [...edges,{
+          source:node.id,
+          target:node.id+node.data.child,
+      }];
+  });
   })
   const onNodesChange = useCallback(
     (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
@@ -47,7 +57,7 @@ function Flow() {
     (connection) => setEdges((eds) => addEdge(connection, eds)),
     [setEdges]
   );
-
+  
   return (
     <ReactFlow
       nodes={nodes}
