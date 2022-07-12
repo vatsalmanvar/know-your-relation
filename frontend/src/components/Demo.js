05bfff763da98da6af8149f38349f121146a6c7e
vatsalmanvar <vatsalmanvar@gmail.com>
2022-07-12 20:20:32 +0530

    adding a child though only button is done, (nodes) is updated but re-redering is not done

--- frontend/src/components/Demo.js
+++ frontend/src/components/Demo.js
@@ -10,6 +10,14 @@ const rfStyle = {
 
 
 const nodeTypes = { textUpdater: TextUpdaterNode };
+let globalNodes = []
+let globalCurrNode = {
+  id:'1', 
+  type: 'textUpdater', 
+  position: { x: 50, y: 50 }, 
+  data: { value: 123, child:0 }, 
+};
+
 
 let vatsal="manvar";
 let textbox;
@@ -18,7 +26,18 @@ function TextUpdaterNode({ data }) {
   const onChange = useCallback((evt) => {
     textbox=evt.target.name;
     vatsal=evt.target.value;
-    //console.log(vatsal,textbox);
+    console.log(vatsal,textbox);
+  }, []);
+
+  const addChild = useCallback((evt)=>{
+    globalCurrNode.data.child = globalCurrNode.data.child+1;
+    globalNodes.push({
+      id: ((parseInt(globalCurrNode.id)*10) + globalCurrNode.data.child).toString(),
+      type:'textUpdater',
+      position:{x:globalCurrNode.position.x,y:globalCurrNode.position.y+100},
+      data:{ value:Math.random(), child:0},
+  });
+    console.log('Clicked', globalNodes);
   }, []);
 
   return (
@@ -27,9 +46,9 @@ function TextUpdaterNode({ data }) {
       <div>
         <label htmlFor="text">Text:</label>
         <input id="text" name="text" onChange={onChange} />
-        <hr></hr>
+        
         <div>
-          <input type="button" value="addChild" />
+          <input type="button" value="addChild" onClick={addChild}/>
           <input type="button" value="Delete"/>
         </div>
       </div>
@@ -39,7 +58,7 @@ function TextUpdaterNode({ data }) {
 }
 
 function Flow() {
-    const [nodes, setNodes] = useState([
+    let [nodes, setNodes] = useState([
       {
         id:'1', 
         type: 'textUpdater', 
@@ -47,7 +66,7 @@ function Flow() {
         data: { value: 123, child:0 }, 
       }
     ]);
-    const [edges, setEdges] = useState([]);  
+    let [edges, setEdges] = useState([]);  
     const [curNode, setCurNode] = useState(
       {
         id:'1', 
@@ -58,51 +77,62 @@ function Flow() {
     );
     //var currentnode = new TextUpdaterNode();
     const FirstNode=()=>{
-      // setNodes((nodes)=>{
-      //   return [...nodes,{
-      //     id:'1', 
-      //     type: 'textUpdater', 
-      //     position: { x: 50, y: 50 }, 
-      //     data: { value: 123, child:0 }, 
-      //   }]
-      // })
+      setNodes((nodes)=>{
+        return [...nodes,{
+          id:'1', 
+          type: 'textUpdater', 
+          position: { x: 50, y: 50 }, 
+          data: { value: 123, child:0 }, 
+        }]
+      })
       console.log('nodes: ',nodes);
     }
 
+    function test(){
+      console.log('test called');
+    }
     
-    
+    // var test = function() {
+    //   console.log('test called');
+    // };
 
-    const x = useCallback((event, node) => {
-      // setCurNode(node);
-      // console.log("CurrNode: ", curNode);
+  //   const onNodeClick = useCallback((event, node) => {
+  //     // setCurNode(node);
+  //     // console.log("CurrNode: ", curNode);
       
-      setNodes((nodes)=>{
-        console.log('clicked node id: ', curNode.id);
-        curNode.data.child = curNode.data.child+1;
-        return [...nodes,{
-            id: ((parseInt(curNode.id)*10) + curNode.data.child).toString(),
-            type:'textUpdater',
-            position:{x:curNode.position.x,y:curNode.position.y+100},
-            data:{ value:Math.random(), child:0},
-        }];
-    },[]);
-    console.log('init nodes',nodes);
+  //     setNodes((nodes)=>{
+  //       console.log('clicked node id: ', curNode.id);
+  //       curNode.data.child = curNode.data.child+1;
+  //       return [...nodes,{
+  //           id: ((parseInt(curNode.id)*10) + curNode.data.child).toString(),
+  //           type:'textUpdater',
+  //           position:{x:curNode.position.x,y:curNode.position.y+100},
+  //           data:{ value:Math.random(), child:0},
+  //       }];
+  //   },[]);
+  //   console.log('init nodes',nodes);
     
-  //   setEdges((edges)=>{
-  //     //console.log('click edge',edges);
-  //     return [...edges,{
-  //         id:Math.random(),
-  //         source:node.id,
-  //         target:node.id+node.data.child,
-  //     }];
-  // });
-  })
+  // //   setEdges((edges)=>{
+  // //     //console.log('click edge',edges);
+  // //     return [...edges,{
+  // //         id:Math.random(),
+  // //         source:node.id,
+  // //         target:node.id+node.data.child,
+  // //     }];
+  // // });
+  // })
   const OnSubmit=()=>{  
     console.log(nodes);
   }  
   const onNodeMouseEnter = useCallback((event, node)=> {
+    globalNodes=nodes;
+    globalCurrNode=node;
     setCurNode(node);
-    console.log("CurrNode: ", curNode);
+    //console.log("CurrNode: ", globalNodes);
+  });
+  const onNodeMouseLeave = useCallback((event, node)=> {
+    nodes = globalNodes;
+    console.log(" --- ------ CurrNode: ", nodes);
   });
   const onNodesChange = useCallback(
     (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
@@ -132,8 +162,10 @@ function Flow() {
     //onNodeClick={onNodeClick}
     fitView
     style={rfStyle}
-    x={x}
+    test={test}
+    //x={x}
     onNodeMouseEnter={onNodeMouseEnter}
+    onNodeMouseLeave={onNodeMouseLeave}
     />
     </>
     );
