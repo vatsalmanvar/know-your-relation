0688e24813b23dea0a3cc8678d3a47101c607bcf
vatsalmanvar <vatsalmanvar@gmail.com>
2022-07-12 18:41:08 +0530

    only one node, onNodeMouseEnter done, addchild not working

--- frontend/src/components/Demo.js
+++ frontend/src/components/Demo.js
@@ -1,62 +1,109 @@
 import { useCallback, useState } from 'react';
 import ReactFlow, { addEdge, applyEdgeChanges, applyNodeChanges } from 'react-flow-renderer';
-
-import TextUpdaterNode from './TextUpdaterNode.js';
-
+import { Handle, Position } from 'react-flow-renderer';
 import './text-updater-node.css';
 
 const rfStyle = {
   backgroundColor: '#B8CEFF',
 };
 
-// const initialNodes = [
-//   ,
-// ];
-// we define the nodeTypes outside of the component to prevent re-renderings
-// you could also use useMemo inside the component
+
+
 const nodeTypes = { textUpdater: TextUpdaterNode };
 
+let vatsal="manvar";
+let textbox;
+
+function TextUpdaterNode({ data }) {
+  const onChange = useCallback((evt) => {
+    textbox=evt.target.name;
+    vatsal=evt.target.value;
+    //console.log(vatsal,textbox);
+  }, []);
+
+  return (
+    <div className="text-updater-node">
+      <Handle type="target" position={Position.Top} />
+      <div>
+        <label htmlFor="text">Text:</label>
+        <input id="text" name="text" onChange={onChange} />
+        <hr></hr>
+        <div>
+          <input type="button" value="addChild" />
+          <input type="button" value="Delete"/>
+        </div>
+      </div>
+      <Handle type="source" position={Position.Bottom} id="b" />
+    </div>
+  );
+}
+
 function Flow() {
-  const [nodes, setNodes] = useState([]);
-  const [edges, setEdges] = useState([]);  
-  const FirstNode=()=>{
-    setNodes((nodes)=>{
-      return [...nodes,{
+    const [nodes, setNodes] = useState([
+      {
         id:'1', 
         type: 'textUpdater', 
         position: { x: 50, y: 50 }, 
         data: { value: 123, child:0 }, 
-      }]
-    })
-    console.log('nodes: ',nodes);
-  }
-  const onNodeClick = useCallback((event, node) => {
-    setNodes((nodes)=>{
-        console.log('clicked node id: ', node.id);
-        node.data.child = node.data.child+1;
+      }
+    ]);
+    const [edges, setEdges] = useState([]);  
+    const [curNode, setCurNode] = useState(
+      {
+        id:'1', 
+        type: 'textUpdater', 
+        position: { x: 50, y: 50 }, 
+        data: { value: 123, child:0 }, 
+      }
+    );
+    //var currentnode = new TextUpdaterNode();
+    const FirstNode=()=>{
+      // setNodes((nodes)=>{
+      //   return [...nodes,{
+      //     id:'1', 
+      //     type: 'textUpdater', 
+      //     position: { x: 50, y: 50 }, 
+      //     data: { value: 123, child:0 }, 
+      //   }]
+      // })
+      console.log('nodes: ',nodes);
+    }
+
+    
+    
+
+    const x = useCallback((event, node) => {
+      // setCurNode(node);
+      // console.log("CurrNode: ", curNode);
+      
+      setNodes((nodes)=>{
+        console.log('clicked node id: ', curNode.id);
+        curNode.data.child = curNode.data.child+1;
         return [...nodes,{
-            id: ((parseInt(node.id)*10) + node.data.child).toString(),
+            id: ((parseInt(curNode.id)*10) + curNode.data.child).toString(),
             type:'textUpdater',
-            position:{x:node.position.x,y:node.position.y+100},
+            position:{x:curNode.position.x,y:curNode.position.y+100},
             data:{ value:Math.random(), child:0},
         }];
     },[]);
     console.log('init nodes',nodes);
-    //console.log('child', node.data.child);
-    //console.log('->>> id', node.id);
-    //console.log('edges', edges);
-    setEdges((edges)=>{
-      console.log('click edge',edges);
-      return [...edges,{
-          id:Math.random(),
-          source:node.id,
-          target:node.id+node.data.child,
-      }];
-  });
+    
+  //   setEdges((edges)=>{
+  //     //console.log('click edge',edges);
+  //     return [...edges,{
+  //         id:Math.random(),
+  //         source:node.id,
+  //         target:node.id+node.data.child,
+  //     }];
+  // });
   })
   const OnSubmit=()=>{  
     console.log(nodes);
   }  
+  const onNodeMouseEnter = useCallback((event, node)=> {
+    setCurNode(node);
+    console.log("CurrNode: ", curNode);
+  });
   const onNodesChange = useCallback(
     (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
     [setNodes]
@@ -69,7 +116,7 @@ function Flow() {
     (connection) => setEdges((eds) => addEdge(connection, eds)),
     [setEdges]
   );
-  
+
   return (
     <>
     <button value="test" onClick={FirstNode}>Start</button>
@@ -82,12 +129,13 @@ function Flow() {
     onEdgesChange={onEdgesChange}
     onConnect={onConnect}
     nodeTypes={nodeTypes}
-    onNodeClick={onNodeClick}
+    //onNodeClick={onNodeClick}
     fitView
     style={rfStyle}
+    x={x}
+    onNodeMouseEnter={onNodeMouseEnter}
     />
     </>
     );
 }
-
-export default Flow;
+export default Flow;     
--- frontend/src/components/TextUpdaterNode.js
+++ frontend/src/components/TextUpdaterNode.js
@@ -1,32 +1,35 @@
-import { useCallback } from 'react';
-import { Handle, Position } from 'react-flow-renderer';
+// import { useCallback } from 'react';
+// import { Handle, Position } from 'react-flow-renderer';
+// const Flow=require('./Demo');
 
-const handleStyle = { left: 10 };
-let vatsal="manvar";
-let textbox;
+// const handleStyle = { left: 10 };
+// let vatsal="manvar";
+// let textbox;
 
-function TextUpdaterNode({ data }) {
-  const onChange = useCallback((evt) => {
-    textbox=evt.target.name;
-    vatsal=evt.target.value;
-    console.log(vatsal,textbox);
-  }, []);
 
-  return (
-    <div className="text-updater-node">
-      <Handle type="target" position={Position.Top} />
-      <div>
-        <label htmlFor="text">Text:</label>
-        <input id="text" name="text" onChange={onChange} />
-        <hr></hr>
-        <div>
-          <input type="button" value="add Child"/>
-        </div>
-      </div>
+// function TextUpdaterNode({ data }) {
+//   const onChange = useCallback((evt) => {
+//     textbox=evt.target.name;
+//     vatsal=evt.target.value;
+//     //console.log(vatsal,textbox);
+//   }, []);
+
+//   return (
+//     <div className="text-updater-node">
+//       <Handle type="target" position={Position.Top} />
+//       <div>
+//         <label htmlFor="text">Text:</label>
+//         <input id="text" name="text" onChange={onChange} />
+//         <hr></hr>
+//         <div>
+//           <input type="button"  />
+//           <input type="button" />
+//         </div>
+//       </div>
       
-      <Handle type="source" position={Position.Bottom} id="b" />
-    </div>
-  );
-}
+//       <Handle type="source" position={Position.Bottom} id="b" />
+//     </div>
+//   );
+// }
 
-export default TextUpdaterNode;
+// export default TextUpdaterNode;
