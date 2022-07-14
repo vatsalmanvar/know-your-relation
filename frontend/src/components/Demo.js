cf26f08ed6f568f4b86f8804739f47969713efa7
vatsalmanvar <vatsalmanvar@gmail.com>
2022-07-14 12:18:20 +0530

    every funtionality is working

--- frontend/src/components/Demo.js
+++ frontend/src/components/Demo.js
@@ -7,41 +7,49 @@ const rfStyle = {
   backgroundColor: '#34495e',
 };
 
-
 const nodeTypes = { textUpdater: TextUpdaterNode };
 
 let globalNodes = [{
   id:'1', 
   type: 'textUpdater', 
   position: { x: 50, y: 50 }, 
-  data: { value: 123, child:0 }, 
-}];
+  data: { value: 123, child:0 ,FN:"xxx", SN:"yyy", male_female:true}}
+];
 let globalCurrNode = {
   id:'1', 
   type: 'textUpdater', 
   position: { x: 50, y: 50 }, 
-  data: { value: 123, child:0 }, 
+  data: { value: 123, child:0, FN:"abc", SN:"def", male_female:true}, 
 };
 let globalEdges = [];
 
 
-let vatsal="manvar";
-let textbox;
-
 function TextUpdaterNode({ data }) {
-  const onChange = useCallback((evt) => {
-    textbox=evt.target.name;
-    vatsal=evt.target.value;
-    console.log(vatsal,textbox);
+  
+  const onChange1 = useCallback((evt) => {
+    globalCurrNode.data.FN=evt.target.value;
+    console.log(globalCurrNode.data.FN);
   }, []);
-
+  const onChange2 = useCallback((evt) => {
+    globalCurrNode.data.SN=evt.target.value;
+    console.log(globalCurrNode.data.SN);
+  }, []);
+  const handleChange = event => {
+    if (event.target.checked) {
+      globalCurrNode.data.male_female = false;
+      console.log('false');
+    } else {
+      globalCurrNode.data.male_female = true;
+      console.log('true');
+    }
+  };
   const addChild = useCallback((evt)=>{
     globalCurrNode.data.child = globalCurrNode.data.child+1;
     globalNodes.push({
       id: globalCurrNode.id + "-" + (globalCurrNode.data.child).toString(),
       type:'textUpdater',
       position:{x:globalCurrNode.position.x, y:globalCurrNode.position.y+100},
-      data:{ value:Math.random(), child:0},
+      data:{ value:Math.random(), child:0, FN:"xxx", SN:"yyy", male_female:true},
     });
     globalEdges.push({
       id: globalCurrNode.id + "-" + (globalCurrNode.data.child).toString(),
@@ -55,28 +63,23 @@ function TextUpdaterNode({ data }) {
     <div className="text-updater-node">
       <Handle type="target" position={Position.Top} />
       <div>
-        
               <div class="flex-container">
                 <div class="flex-child magenta">
                   <label class="text" htmlFor="text" >First Person</label>
-                  <input class="textcss" id="firstPersonName" name="text" onChange={onChange} />
-                  <input class="radio" type="radio" value="Male-Female" name="gender" /> Male-Female
-                  <br></br>
-                  <input class="button" id="addChildButton" type="button" value="Child" onClick={addChild}/>
+                  <input class="textcss"id="firstPersonName" name="text" onChange={onChange1} />
                 </div>
 
                 <div class="flex-child green">
                   <label class="text" htmlFor="text">Second Person</label>
-                  <input class="textcss" id="secondPersonName" name="text" onChange={onChange} />
-                  <input class="radio" type="radio" value="Female-Male" name="gender" /> Female-Male
-                  <br></br>
-                  <input class="button" type="button" value="Delete"/>
-                
+                  <input class="textcss" id="secondPersonName" name="text" onChange={onChange2} />
                 </div>
               </div>
               
               <div>
-                
+              <input type="checkbox" onClick={handleChange}/>Is First Person Female
+              <input class="deletebutton" type="button" value="Delete Node"/>
+              <br></br>
+              <input class="addchildbutton" id="addChildButton" type="button" value="Add Child" onClick={addChild}/>
               </div>
 
         <div>
@@ -100,23 +103,24 @@ function Flow() {
     setEdges((edges)=>{
       return [...globalEdges,];
     },[]); 
-    console.log("onNodeClicked");
-    console.log(nodes);
-    console.log(edges);
   })
   
-
-  const onNodeDragStop = useCallback((event, node) => {
-    globalNodes.map((item)=>{
-      if(node.id === item.id) {
-        item.position.x = node.position.x;
-        item.position.y = node.position.y;
-      }
-    });
-    console.log('onNodeDragStop');
-    
+  
+  const onNodeDrag = useCallback((event, node) => {
+    globalCurrNode.position.x = node.position.x;
+    globalCurrNode.position.y = node.position.y;
+    console.log('onNodeDrag');
   })
 
+  const onNodeDragStop = useCallback((event, node) => {
+    globalCurrNode.position.x = node.position.x;
+    globalCurrNode.position.y = node.position.y;
+    setNodes((nodes)=>{
+      return [...globalNodes,];
+    },[]);
+    
+    console.log('onNodeDragStop');
+  })
 
   const onNodeMouseEnter = useCallback((event, node)=> {
     globalCurrNode=node;
@@ -131,7 +135,7 @@ function Flow() {
 
 
   const onNodesChange = useCallback(
-    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+    (changes) => setNodes((nds) => applyNodeChanges(changes, globalNodes)),
     [setNodes]
   );
   const onEdgesChange = useCallback(
@@ -143,8 +147,16 @@ function Flow() {
     [setEdges]
   );
 
+  const printgnodes = useCallback((evt) => {
+    globalNodes.map((item)=>{
+      console.log(item.id, item.data.FN, item.data.SN, item.data.male_female);
+    });
+    console.log(globalEdges);
+  }, []);
+
   return (
     <>
+    <button onClick={printgnodes}>Print All nodes details</button>
     <ReactFlow
     nodes={nodes}
     edges={edges}
@@ -157,9 +169,7 @@ function Flow() {
 
     onNodeClick={onNodeClick}
     onNodeMouseEnter={onNodeMouseEnter}
-    //onNodeMouseMove={onNodeMouseMove}
     onNodeMouseLeave={onNodeMouseLeave}
-    //onNodeDragStart={onNodeDragStart}
     //onNodeDrag={onNodeDrag}
     onNodeDragStop={onNodeDragStop}
     />
