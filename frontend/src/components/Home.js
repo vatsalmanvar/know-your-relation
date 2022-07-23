0daf1831a0308c0f8663464e7b60d794d1c94579
vatsalmanvar <vatsalmanvar@gmail.com>
2022-07-23 17:50:42 +0530

    UI updated

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -59,27 +59,55 @@ function Home() {
           <a href="/signin"><button className="btn btn-secondary float-end me-4" >Sign in</button></a>
           <a href="/register"><button className="btn btn-secondary float-end me-2">Register</button></a>
         </div>
-        <span className='my-5' style={{fontFamily : 'fantasy'}}><h5>Welcome to</h5><h1>Know Your Relation</h1></span>
+        <span><h6>Welcome to</h6></span>
+        <span className='my-5' style={{fontFamily : 'fantasy'}}><h1>Know Your Relation</h1></span>
       </div>
       </div>
       
       
 
-      <div className="container" >
-        <form className="form-inlineup">
-        
-        <input type="text"  name="familyName" className="form-control" id="exampleInputEmail1" onChange={handleInputs} aria-describedby="Help" placeholder="Enter family name"/>
-        <button type="submit" className="btn btn-primary mt-3" onClick={submit}>View</button>
-        
-        </form>
-      <div>
-      <img src='https://wallpaperaccess.com/full/320438.jpg' width={960} height={600} class="rounded mx-auto d-block" style={{boxSizing: "5px"}}></img>
-      </div>
+     
+    <section className="vh-100" style={{"backgroundColor": "#eee"}}>
+  <div className="container h-100">
+    <div className="row d-flex justify-content-center align-items-center h-100">
+      <div className="col-lg-12 col-xl-11">
+        <div className="card text-black" style={{"borderRadius": "25px"}}>
+          <div className="card-body p-md-5">
+            <div className="row justify-content-center">
+              <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
 
+                <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Enter Family Name and View Tree</p>
+
+                <form className="mx-1 mx-md-4">
+
+
+                  <div className="d-flex flex-row align-items-center mb-4">
+                    <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                    <div className="form-outline flex-fill mb-0">
+                      <input type="text" id="exampleInputEmail1" name="familyName" className="form-control" placeholder='Enter Family Name' onChange={handleInputs}/>
+                    </div>
+                  </div> 
+
+                  <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                    <button type="submit" className="btn btn-primary btn-lg" onClick={submit}>View Tree</button>
+                  </div>
+
+                </form>
+
+              </div>
+              <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+
+                <img src="https://wallpaperaccess.com/full/320438.jpg"
+                  className="img-fluid" alt="demo"/>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    
-    
-    
+  </div>
+</section>
     </>
   )
 }
